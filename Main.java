3b2211b761a3cc5940a19bacce7abcc8b10df570
SakshiPatel-20 <141473943+SakshiPatel-20@users.noreply.github.com>
2023-09-26 15:45:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String args[])
+	{
+		City c1 = new City();
+		c1.setCity_name("Ahmedabad");
+		c1.setCountry_name("India");
+		c1.setState_name("Gujarat");
+		
+		System.out.println(c1.toString());
+	}
+}
